e0f01920d48b44622d87ce1e1c26888989559467
pharo <sec.pharo@gmail.com>
2022-06-12 14:32:08 -0400

    added color variables

--- AutoBox.sh
+++ AutoBox.sh
@@ -4,48 +4,61 @@
 #!/bin/bash
 # tputcolors
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Creating Directory: $(tput setaf 2)$1"
-mkdir $1
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+WHITE=$(tput setaf 7)
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Creating Sub-Directory: $(tput setaf 2)$1/nmap"
-mkdir $1/nmap
+if [ $# -eq 0 ]; then
+    echo "$RED[+]$WHITE No Name Provided"
+    read -p "$YELLOW[+]$WHITE Please Provide the Machine Name: " BOX
+else
+	BOX = $1
+fi
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Creating Sub-Directory: $(tput setaf 2)$1/enumeration"
-mkdir $1/enumeration
+echo "$BLUE[+]$WHITE Creating Directory: $(tput setaf 2)$BOX"
+mkdir $BOX
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Creating Sub-Directory: $(tput setaf 2)$1/shell"
-mkdir $1/shell
+echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/nmap"
+mkdir $BOX/nmap
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Creating Sub-Directory: $(tput setaf 2)$1/priv-esc"
-mkdir $1/priv-esc
+echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/enumeration"
+mkdir $BOX/enumeration
 
-read -p "$(tput setaf 3)[+] $(tput setaf 7)Enter Host IP for Intial Enumeration: " IP
+echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/shell"
+mkdir $BOX/shell
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Running Full Port Scan on $IP"
+echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/priv-esc"
+mkdir $BOX/priv-esc
 
-nmap -p- -Pn $IP -oA $1/nmap/full-port --open > /dev/null 2>&1
+read -p "$YELLOW[+]$WHITE Enter Host IP for Intial Enumeration: " IP
 
-echo "$(tput setaf 2)[+] Done!"
+echo "$BLUE[+]$WHITE Running Full Port Scan on $IP"
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Output Saved To: $(tput setaf 2)$1/nmap/full-port.*" 
+nmap -p- -Pn $IP -oA $BOX/nmap/full-port --open > /dev/null 2>&1
 
-echo "$(tput setaf 7)$(cat $1/nmap/full-port.nmap | grep open | grep -v Nmap | cut -f 1 -d '/' > $1/nmap/open-ports.txt)"
+echo "$GREEN[+] Done!"
 
-echo "$(tput setaf 1)[+] $(tput setaf 7)Ports Found Open:"
-echo "$(tput setaf 7)$(cat $1/nmap/open-ports.txt)"
+echo "$BLUE[+]$WHITE Output Saved To:$GREEN $BOX/nmap/full-port.*" 
+
+echo "$WHITE$(cat $BOX/nmap/full-port.nmap | grep open | grep -v Nmap | cut -f 1 -d '/' > $BOX/nmap/open-ports.txt)"
+
+echo "$GREEN[+]$WHITE Ports Found Open:"
+echo "$WHITE$(cat $BOX/nmap/open-ports.txt)"
 echo ""
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Running a Service Scan on the Open Ports"
-ports=$(sed -z 's/\n/,/g' $1/nmap/open-ports.txt | sed '$ s/.$//')
+echo "$BLUE[+]$WHITE Running a Service Scan on the Open Ports"
+ports=$(sed -z 's/\n/,/g' $BOX/nmap/open-ports.txt | sed '$ s/.$//')
 
-nmap -p $ports -Pn -sC -sV -oA $1/nmap/service-scan $IP > /dev/null 2>&1
-echo "$(tput setaf 2)[+] Done!"
+nmap -p $ports -Pn -sC -sV -oA $BOX/nmap/service-scan $IP > /dev/null 2>&1
+echo "$GREEN[+] Done!"
 
-echo "$(tput setaf 4)[+] $(tput setaf 7)Output Saved To: $(tput setaf 2)$1/nmap/service-scan.*"
+echo "$BLUE[+]$WHITE Output Saved To:$GREEN $BOX/nmap/service-scan.*"
 
 echo ""
-echo "$(tput setaf 1)[+] $(tput setaf 7)Services/Versions Identified:"
-echo "$(tput setaf 7)$(cat $1/nmap/service-scan.nmap | grep tcp)"
+echo "$GREEN[+]$WHITE Services/Versions Identified:"
+echo "$WHITE$(cat $BOX/nmap/service-scan.nmap | grep tcp)"
 
-echo "$(tput setaf 2)Happy Hacking!"
+echo $GREEN"Happy Hacking!"
 
