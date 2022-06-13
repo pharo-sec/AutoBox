1792ef531974bbeba62b6bafc2112e0cb82d42c2
pharo <sec.pharo@gmail.com>
2022-06-13 19:48:25 -0400

    added spinning animation on nmaps

--- AutoBox.sh
+++ AutoBox.sh
@@ -1,6 +1,3 @@
-#Author: PharoSec
-#Description: Automates the basic startup tasks when started a new pentest box
-
 #!/bin/bash
 # tputcolors
 
@@ -14,51 +11,75 @@ if [ $# -eq 0 ]; then
     echo "$RED[+]$WHITE No Name Provided"
     read -p "$YELLOW[+]$WHITE Please Provide the Machine Name: " BOX
 else
-	BOX = $1
+        BOX=$1
 fi
 
-echo "$BLUE[+]$WHITE Creating Directory:$GREEN $BOX"
-mkdir $BOX
+if [ -d "$BOX" ]; then
+    echo "$RED[+]$WHITE Directory Already Exists!"
+else
+    echo "$BLUE[+]$WHITE Creating Directory:$GREEN $BOX"
+    mkdir $BOX
 
-echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/nmap"
-mkdir $BOX/nmap
+    echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/nmap"
+    mkdir $BOX/nmap
 
-echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/enumeration"
-mkdir $BOX/enumeration
+    echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/enumeration"
+    mkdir $BOX/enumeration
 
-echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/shell"
-mkdir $BOX/shell
+    echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/shell"
+    mkdir $BOX/shell
 
-echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/priv-esc"
-mkdir $BOX/priv-esc
+    echo "$BLUE[+]$WHITE Creating Sub-Directory:$GREEN $BOX/priv-esc"
+    mkdir $BOX/priv-esc
+fi
 
 read -p "$YELLOW[+]$WHITE Enter Host IP for Intial Enumeration: " IP
 
 echo "$BLUE[+]$WHITE Running Full Port Scan on $IP"
 
-nmap -p- -Pn $IP -oA $BOX/nmap/full-port --open > /dev/null 2>&1
-
-echo "$GREEN[+] Done!"
+nmap $IP -p- -oA $BOX/nmap/full-port --open -Pn > /dev/null &
+PID=$!
+i=1
+sp="/-\|"
+echo -n ' '
+while [ -d /proc/$PID ]; do
+  printf "\b${sp:i++%${#sp}:1}"
+  sleep 0.2
+done
+printf "\b$GREEN[+]$WHITE Done!\n"
 
 echo "$BLUE[+]$WHITE Output Saved To:$GREEN $BOX/nmap/full-port.*" 
 
 echo "$WHITE$(cat $BOX/nmap/full-port.nmap | grep open | grep -v Nmap | cut -f 1 -d '/' > $BOX/nmap/open-ports.txt)"
 
-echo "$GREEN[+]$WHITE Ports Found Open:"
-echo "$WHITE$(cat $BOX/nmap/open-ports.txt)"
-echo ""
+if [ -s $BOX/nmap/open-ports.txt ]; then
+        echo "$GREEN[+]$WHITE Ports Found Open:"
+        echo "$WHITE$(cat $BOX/nmap/open-ports.txt)"
+        echo ""
 
-echo "$BLUE[+]$WHITE Running a Service Scan on the Open Ports"
-ports=$(sed -z 's/\n/,/g' $BOX/nmap/open-ports.txt | sed '$ s/.$//')
+        echo "$BLUE[+]$WHITE Running a Service Scan on the Open Ports"
+        ports=$(sed -z 's/\n/,/g' $BOX/nmap/open-ports.txt | sed '$ s/.$//')
 
-nmap -p $ports -Pn -sC -sV -oA $BOX/nmap/service-scan $IP > /dev/null 2>&1
-echo "$GREEN[+] Done!"
+        nmap $IP -p $ports -sC -sV -oA $BOX/nmap/service-scan -Pn > /dev/null &
+        PID=$!
+        i=1
+        sp="/-\|"
+        echo -n ' '
+        while [ -d /proc/$PID ]; do
+                printf "\b${sp:i++%${#sp}:1}"
+                sleep 0.2
+        done
+        printf "\b$GREEN[+]$WHITE Done!\n"
+        echo "$GREEN[+] Done!"
 
-echo "$BLUE[+]$WHITE Output Saved To:$GREEN $BOX/nmap/service-scan.*"
+        echo "$BLUE[+]$WHITE Output Saved To:$GREEN $BOX/nmap/service-scan.*"
 
-echo ""
-echo "$GREEN[+]$WHITE Services/Versions Identified:"
-echo "$WHITE$(cat $BOX/nmap/service-scan.nmap | grep tcp)"
+        echo ""
+        echo "$GREEN[+]$WHITE Services/Versions Identified:"
+        echo "$WHITE$(cat $BOX/nmap/service-scan.nmap | grep tcp)"
+else
+        echo "$RED[+]$WHITE No Ports Open...Skipping Service Scan"
+fi 
 
+echo ' '
 echo $GREEN"Happy Hacking!"
-
